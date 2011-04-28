d086fa5844656ad0523c1dd27f72cbcba8608ffa
alvaromartinorive@gmail.com <alvaromartinorive@gmail.com@56eaa8f5-34cf-fb75-7609-e325a0b7f16d>
2011-04-28 09:52:43 +0000

    Corregidos los errores de las constraints

--- trunk/TimeEquationCSP/src/es/deusto/ingenieria/aike/TimeEquation/Add.java
+++ trunk/TimeEquationCSP/src/es/deusto/ingenieria/aike/TimeEquation/Add.java
@@ -22,26 +22,26 @@ public class Add extends Constraint<Integer> {
 		if ( ( variable!= null )&& ( this.getVariables().contains(variable) ) )
 			
 			switch (addType) {
-			case 2: // D x Multiplier = Constant + 10 x X1
-				
-				if ( this.getVariables().get(0).getValue() * multiplier == constant + 10 * this.getVariables().get(1).getValue() )
-					return true;
-				else return false;
-				
-			case 3: // A x Multiplier = E + X3 
-				
-				if ( this.getVariables().get(0).getValue() * multiplier == this.getVariables().get(1).getValue() + this.getVariables().get(2).getValue() )
-					return true;
-				else return false;
-				
-			case 4: // B x Multiplier = F + X2 + 10 x X3 OR C x Multiplier = G + X1 + 10 x X2
-				
-				if ( this.getVariables().get(0).getValue() * multiplier == this.getVariables().get(1).getValue() + this.getVariables().get(2).getValue() + 10 * this.getVariables().get(3).getValue() )
-					return true;
-				else return false;
-				
-			default: System.out.println("Error, the add constraint does not have a correct type");
-				return false;
+				case 2: // D x Multiplier = Constant + 10 x X1
+					
+					if ( this.getVariables().get(0).getValue() * multiplier == constant + 10 * this.getVariables().get(1).getValue() )
+						return true;
+					else return false;
+					
+				case 3: // A x Multiplier = E + X3 
+					
+					if ( this.getVariables().get(0).getValue() * multiplier == this.getVariables().get(1).getValue() + this.getVariables().get(2).getValue() )
+						return true;
+					else return false;
+					
+				case 4: // B x Multiplier = F + X2 + 10 x X3 OR C x Multiplier = G + X1 + 10 x X2
+					
+					if ( this.getVariables().get(0).getValue() * multiplier == this.getVariables().get(1).getValue() + this.getVariables().get(2).getValue() + 10 * this.getVariables().get(3).getValue() )
+						return true;
+					else return false;
+					
+				default: System.out.println("Error, the add constraint does not have a correct type");
+					return false;
 			}
 		else return false;
 		
