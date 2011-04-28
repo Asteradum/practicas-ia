b0c187d5f48fdc4ee794e867c198f4a9793412b0
corazondetango28 <corazondetango28@56eaa8f5-34cf-fb75-7609-e325a0b7f16d>
2011-04-28 16:12:59 +0000

    NodeConsist..

--- trunk/TimeEquationCSP/src/es/deusto/ingenieria/aike/TimeEquation/ConstraintPropagation.java
+++ trunk/TimeEquationCSP/src/es/deusto/ingenieria/aike/TimeEquation/ConstraintPropagation.java
@@ -1,6 +1,11 @@
 package es.deusto.ingenieria.aike.TimeEquation;
 
+import java.util.List;
+
+import es.deusto.ingenieria.aike.csp.formulation.Variable;
+
 public interface ConstraintPropagation {
 	
-	public void makeConsistent();
+	
+	public TimeEquationProblem makeConsistent(TimeEquationProblem problem);
 }
