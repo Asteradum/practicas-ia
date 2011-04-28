3d2900135d2aa16a8295ed95ab60a14312948544
corazondetango28 <corazondetango28@56eaa8f5-34cf-fb75-7609-e325a0b7f16d>
2011-04-28 16:12:59 +0000

    NodeConsist..

--- TimeEquationCSP/src/es/deusto/ingenieria/aike/TimeEquation/NodeConsistency.java
+++ TimeEquationCSP/src/es/deusto/ingenieria/aike/TimeEquation/NodeConsistency.java
@@ -1,13 +1,32 @@
 package es.deusto.ingenieria.aike.TimeEquation;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import es.deusto.ingenieria.aike.csp.formulation.Variable;
+
 public class NodeConsistency implements ConstraintPropagation {
 
 	@Override
-	public void makeConsistent() {
+	public TimeEquationProblem makeConsistent(TimeEquationProblem problem) {
 		// TODO Auto-generated method stub
 		
+		for (int i=0;i<problem.getVariables().size();i++)
+		{ List<Variable<Integer>> lista=problem.getVariables();
+			
+		if ((lista.get(i).getName().equals("D"))&& (lista.get(i).getName().equals("G")))
+				{System.out.println(lista.get(i).getName());
+				List<Integer> domain = new ArrayList<Integer>(6);		
+				for (int j=1; j<=5; j++) 
+					domain.add(j);
+				lista.get(i).setDomain(domain);
+				}
+		
+		}
+		return problem;
 		//Para checkear si es una constraint unaria solo hace falta preguntar si la constraint asociada a la variable es de tipo UnaryConstraint
 		
+	
 	}
 
 }
