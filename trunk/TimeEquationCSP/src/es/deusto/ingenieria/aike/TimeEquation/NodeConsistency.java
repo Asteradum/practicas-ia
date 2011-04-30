fd98b1a4d0c4d118ffe608bb5787e4033bef555e
corazondetango28 <corazondetango28@56eaa8f5-34cf-fb75-7609-e325a0b7f16d>
2011-04-30 15:16:58 +0000

    nodeConsistency

--- trunk/TimeEquationCSP/src/es/deusto/ingenieria/aike/TimeEquation/NodeConsistency.java
+++ trunk/TimeEquationCSP/src/es/deusto/ingenieria/aike/TimeEquation/NodeConsistency.java
@@ -3,30 +3,50 @@ package es.deusto.ingenieria.aike.TimeEquation;
 import java.util.ArrayList;
 import java.util.List;
 
+import es.deusto.ingenieria.aike.csp.formulation.Constraint;
 import es.deusto.ingenieria.aike.csp.formulation.Variable;
 
 public class NodeConsistency implements ConstraintPropagation {
 
 	@Override
-	public TimeEquationProblem makeConsistent(TimeEquationProblem problem) {
+	public void makeConsistent(TimeEquationProblem problem) {
 		// TODO Auto-generated method stub
-		
-		for (int i=0;i<problem.getVariables().size();i++)
-		{ List<Variable<Integer>> lista=problem.getVariables();
-			
-		if ((lista.get(i).getName().equals("D"))&& (lista.get(i).getName().equals("G")))
-				{System.out.println(lista.get(i).getName());
-				List<Integer> domain = new ArrayList<Integer>(6);		
-				for (int j=1; j<=5; j++) 
-					domain.add(j);
-				lista.get(i).setDomain(domain);
+		//tengo todas las variables con todos sus dominios
+		for (int i=0;i<problem.getVariables().size();i++){
+			List<Variable<Integer>> lista=problem.getVariables();
+			//hago una lista con las constraints de cada variable
+			List<Constraint<Integer>> listConst=lista.get(i).getConstraints();
+			for (int j=0;j<listConst.size();j++){
+				if (listConst.get(j).getName().equals("DistinctFrom")){
+					//System.out.println(lista.get(i).getValue());
+					//solo estan asignados los valores del multiplicador y de la constante
+					//no se que valor quieres q quite del dominio, no se puede acceder a Value
+					//xq todavia no esta asignado.Aparte no hay un RemoveValue asiq el dominio
+					//hay q volver a crearlo borrando el anterior
+					List<Integer> domain = new ArrayList<Integer>(9);
+					int multiplier=lista.get(4).getValue();
+					int constant=lista.get(8).getValue();
+					int d=1;
+					while (d<10){
+						if ((d!=multiplier) &&(d!=constant))
+							domain.add(d);
+						d++;
+					}
+					lista.get(i).setDomain(domain);
 				}
-		
+				if (listConst.get(j).getName().equals("EqualTo")){
+					
+				}
+				if (listConst.get(j).getName().equals("LowerThan")){
+					List<Integer> domain = new ArrayList<Integer>(6);		
+					for (int dom=1; dom<=5; dom++) 
+						domain.add(dom);
+					lista.get(i).setDomain(domain);
+				}
+			}
+					
 		}
-		return problem;
-		//Para checkear si es una constraint unaria solo hace falta preguntar si la constraint asociada a la variable es de tipo UnaryConstraint
 		
-	
 	}
 
 }
